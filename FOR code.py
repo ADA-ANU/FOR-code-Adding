8bac6b09f440071172ef3dcfeec506ea4b727bca
Mingjing <pholmesp>
2020-03-10 11:35:07 +1100

    Multiple FOR codes supported 10 Mar 2020

--- FOR code.py
+++ FOR code.py
@@ -31,7 +31,7 @@ def readCSV():
             else:
                 line_count += 1
                 print(row[3], row[4])
-                fetchDataset(row[3], row[4])
+                fetchDataset(row[3], row[4].split())
         print(f'Processed {line_count} lines.')
         writeResult()
 
@@ -55,10 +55,6 @@ def writeResult():
         writer.writerow(["Other error: "])
         for i in unKnownError:
             writer.writerow(i)
-        writer.writerow([])
-        writer.writerow(["Draft: "])
-        for i in draftUpdate:
-            writer.writerow(i)
 
     f = open('success.csv', 'w')
     with f:
@@ -66,89 +62,91 @@ def writeResult():
         writer.writerow(["Successfully updated: "])
         for i in successDatasets:
             writer.writerow(i)
+        writer.writerow([])
+        writer.writerow(["Draft: "])
+        for i in draftUpdate:
+            writer.writerow(i)
 
 
-def fetchDataset(doi, forCode):
+def fetchDataset(doi, forCodes):
     entries = (
         'id', 'versionNumber', 'versionMinorNumber', 'versionState', 'productionDate', 'lastUpdateTime', 'releaseTime',
         'createTime', 'UNF', 'files')
     try:
         r = requests.get(Constants.API_FETCHDATASET + doi, headers=Constants.API_WP_POSTS_HEADER)
         print("fetch " + str(r.status_code))
-        print(json.loads(r.text))
-        # if r.status_code == 200 or r.status_code == 201:
-        #     r.encoding = "utf-8"
-        #     res = json.loads(r.text)
-        #     if 'latestVersion' in res['data']:
-        #         updateJSON = res['data']['latestVersion']
-        #         status = res['data']['latestVersion']['versionState']
-        #     else:
-        #         logging(doi, 'unknown', "key latestVersion doesn't exist, please check its status.")
-        #         return
-        #     for k in entries:
-        #         updateJSON.pop(k, None)
-        #     fields = editDataset(updateJSON, forCode)
-        #     updateJSON['metadataBlocks']['citation']['fields'] = fields
-        #     updateDataset(doi, updateJSON, status)
-        # else:
-        #     logging(doi, 'fetch', json.loads(r.text))
+        # print(json.loads(r.text))
+        if r.status_code == 200 or r.status_code == 201:
+            r.encoding = "utf-8"
+            res = json.loads(r.text)
+            if 'latestVersion' in res['data']:
+                updateJSON = res['data']['latestVersion']
+                status = res['data']['latestVersion']['versionState']
+            else:
+                logging(doi, 'unknown', "key latestVersion doesn't exist, the dataset could have been deaccessioned.")
+                return
+            for k in entries:
+                updateJSON.pop(k, None)
+            fields = editDataset(updateJSON, forCodes)
+            updateJSON['metadataBlocks']['citation']['fields'] = fields
+            updateDataset(doi, updateJSON, status)
+        else:
+            logging(doi, 'fetch', json.loads(r.text))
     except Exception as error:
         print('ERROR', error)
 
 
-def editDataset(dataset, forCode):
+def editDataset(dataset, forCodes):
     fields = dataset['metadataBlocks']['citation']['fields']
     topicJSON = {"typeName": "topicClassification",
                  "multiple": True,
                  "typeClass": "compound",
                  "value": [
-                     {"topicClassValue":
-                         {
-                             "typeName": "topicClassValue",
-                             "multiple": False,
-                             "typeClass": "primitive",
-                             "value": forCode
-                         }
-                      }
+
                  ]
                  }
 
-    for i in fields:
-        if i['typeName'] == 'topicClassification':
-            if i['multiple']:
-                i['value'][0]['topicClassValue']['value'] = forCode
-                print("Found topicClassification true")
-                return fields
-            else:
-                i['value']['topicClassValue']['value'] = forCode
-                print("Found topicClassification false")
-                return fields
+    for forCode in forCodes:
+        topicClassValue = {"topicClassValue":
+            {
+                "typeName": "topicClassValue",
+                "multiple": False,
+                "typeClass": "primitive",
+                "value": forCode
+            }
+        }
+        topicJSON['value'].append(topicClassValue)
+
+    for i in range(len(fields)):
+        if fields[i]['typeName'] == 'topicClassification':
+            fields[i] = topicJSON
+            return fields
+
     print("not found")
-    topicJSON['value'][0]['topicClassValue']['value'] = forCode
     fields.append(topicJSON)
     return fields
 
 
 def logging(doi, stage, info):
+    url = 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId='
     if stage == 'fetch':
-        fetchError.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId='+doi, info])
+        fetchError.append([doi, url + doi, info])
     elif stage == 'update':
-        updateError.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId='+doi, info])
+        updateError.append([doi, url + doi, info])
     elif stage == 'publish':
-        publishError.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId='+doi, info])
+        publishError.append([doi, url + doi, info])
     elif stage == 'draft':
-        draftUpdate.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId=' + doi, info])
+        draftUpdate.append([doi, url + doi, info])
     elif stage == 'success':
-        successDatasets.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId=' + doi])
+        successDatasets.append([doi, url + doi])
     else:
         print("unknown error")
-        unKnownError.append([doi, 'https://dataverse-dev.ada.edu.au/dataset.xhtml?persistentId='+doi, info])
+        unKnownError.append([doi, url + doi, info])
 
     print("Log saved.")
 
 
 def updateDataset(doi, data, status):
-
     try:
         tempD = json.dumps(data, ensure_ascii=True).encode('UTF-8')
     except Exception as error:
@@ -192,8 +190,8 @@ def publishDataset(doi):
 
 def main():
     print(str(currentDateTime()) + " Executing...")
-    # readCSV()
-    fetchDataset('doi:10.5072/82/B93ZD5', 1)
+    readCSV()
+    # fetchDataset('doi:10.5072/82/B93ZD5', 1)
 
 
 if __name__ == "__main__":
